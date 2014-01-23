5d62eefbfd2093cb92b5539e35b25de6b405555b
Michael Toth <michael@virtualpianist.com>
2014-01-23 18:33:36 -0500

    Initial Commit

---
+++ RandomPossessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomPossessions
+//
+//  Created by TRUM147 iMac2 on 1/23/14.
+//  Copyright (c) 2014 TRUM147 iMac2. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
